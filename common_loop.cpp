55001c73eafd4bc8fda667f80e7585dbed2fc8ca
yangshuo <17801003317@163.com>
2023-02-20 00:34:49 +0800

    增加日志库

---
+++ common_loop.cpp
@@ -0,0 +1,38 @@
+#include "common_loop.h"
+#include "log.h"
+
+void* CommonLooper::trampoline(void *p)
+{
+    LogInfo("into")
+}
+
+
+CommonLooper::CommonLooper(/* args */)
+{
+
+}
+CommonLooper::~CommonLooper()
+{
+
+}
+
+RET_CODE CommonLooper::Start()
+{
+
+}
+
+void CommonLooper::Stop()
+{
+
+
+}
+
+bool CommonLooper::Running()
+{
+    
+}
+
+void CommonLooper::SetRunning(bool running)
+{
+
+}
--- common_loop.h
+++ common_loop.h
@@ -14,21 +14,18 @@ public:
 
     virtual RET_CODE Start();  // 开启线程
     virtual void Stop();  // 停止线程
+    virtual bool Running();
+    virtual void SetRunning(bool running);
     virtual void Loop() = 0;
 
 private:
     static void* trampoline(void *p);
 
 protected:
+    std::this_thread *worker_ = nullptr;  // 线程
+    bool request_abort_ = false;  // 请求退出线程的标志
+    bool running_ =  true;  // 线程是否在运行
 };
-
-CommonLooper::CommonLooper(/* args */)
-{
-}
-
-CommonLooper::~CommonLooper()
-{
-}
 
 
 #endif // _COMMON_LOOP_H_
